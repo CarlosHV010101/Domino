662d269ddf7560977eb46e74c9196170b8729974
Carlos Hernandez <chernandez@teknei.com>
2023-03-12 12:38:04 -0600

    Initial Commit

---
+++ Domino/DominoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DominoApp.swift
+//  Domino
+//
+//  Created by Administrador on 12/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DominoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
